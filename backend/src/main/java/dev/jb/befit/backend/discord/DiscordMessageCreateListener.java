60a3248e4115c50d950c1f11c0413d2670b0f49d
jordy <74408184+jordybronowicki37@users.noreply.github.com>
2024-03-12 23:01:02 +0100

    Removed abstract message listener

--- backend/src/main/java/dev/jb/befit/backend/discord/DiscordMessageCreateListener.java
+++ backend/src/main/java/dev/jb/befit/backend/discord/DiscordMessageCreateListener.java
@@ -1,11 +1,12 @@
 package dev.jb.befit.backend.discord;
 
 import discord4j.core.event.domain.message.MessageCreateEvent;
+import discord4j.core.object.entity.Message;
 import org.springframework.stereotype.Service;
 import reactor.core.publisher.Mono;
 
 @Service
-public class DiscordMessageCreateListener extends DiscordMessageListener implements DiscordEventListener<MessageCreateEvent> {
+public class DiscordMessageCreateListener implements DiscordEventListener<MessageCreateEvent> {
 
     @Override
     public Class<MessageCreateEvent> getEventType() {
@@ -14,6 +15,11 @@ public class DiscordMessageCreateListener extends DiscordMessageListener impleme
 
     @Override
     public Mono<Void> execute(MessageCreateEvent event) {
-        return processCommand(event.getMessage());
+        return Mono.just(event.getMessage())
+                .filter(message -> message.getAuthor().map(user -> !user.isBot()).orElse(false))
+                .filter(message -> message.getContent().equalsIgnoreCase("!todo"))
+                .flatMap(Message::getChannel)
+                .flatMap(channel -> channel.createMessage("Things to do today:\n - write a bot\n - eat lunch\n - play a game"))
+                .then();
     }
 }
--- backend/src/main/java/dev/jb/befit/backend/discord/DiscordMessageUpdateListener.java
+++ backend/src/main/java/dev/jb/befit/backend/discord/DiscordMessageUpdateListener.java
@@ -1,11 +1,12 @@
 package dev.jb.befit.backend.discord;
 
 import discord4j.core.event.domain.message.MessageUpdateEvent;
+import discord4j.core.object.entity.Message;
 import org.springframework.stereotype.Service;
 import reactor.core.publisher.Mono;
 
 @Service
-public class DiscordMessageUpdateListener extends DiscordMessageListener implements DiscordEventListener<MessageUpdateEvent> {
+public class DiscordMessageUpdateListener implements DiscordEventListener<MessageUpdateEvent> {
 
     @Override
     public Class<MessageUpdateEvent> getEventType() {
@@ -17,6 +18,10 @@ public class DiscordMessageUpdateListener extends DiscordMessageListener impleme
         return Mono.just(event)
                 .filter(MessageUpdateEvent::isContentChanged)
                 .flatMap(MessageUpdateEvent::getMessage)
-                .flatMap(super::processCommand);
+                .filter(message -> message.getAuthor().map(user -> !user.isBot()).orElse(false))
+                .filter(message -> message.getContent().equalsIgnoreCase("!todo"))
+                .flatMap(Message::getChannel)
+                .flatMap(channel -> channel.createMessage("Things to do today:\n - write a bot\n - eat lunch\n - play a game"))
+                .then();
     }
 }
