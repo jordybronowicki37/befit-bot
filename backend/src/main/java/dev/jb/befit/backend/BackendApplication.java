86b6c12a29ed01923ec310decea3d26e55a38bee
jordy <74408184+jordybronowicki37@users.noreply.github.com>
2024-03-15 15:14:31 +0100

    Added scheduled motivational quote

--- backend/src/main/java/dev/jb/befit/backend/BackendApplication.java
+++ backend/src/main/java/dev/jb/befit/backend/BackendApplication.java
@@ -1,11 +1,21 @@
 package dev.jb.befit.backend;
 
+import jakarta.annotation.PostConstruct;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+import java.util.TimeZone;
 
 @SpringBootApplication
+@EnableScheduling
 public class BackendApplication {
     public static void main(String[] args) {
         SpringApplication.run(BackendApplication.class, args);
+    }
+
+    @PostConstruct
+    public void init() {
+        TimeZone.setDefault(TimeZone.getTimeZone("Europe/Amsterdam"));
     }
 }
---
+++ backend/src/main/java/dev/jb/befit/backend/discord/jobs/ScheduledMotivationalQuote.java
@@ -0,0 +1,43 @@
+package dev.jb.befit.backend.discord.jobs;
+
+import dev.jb.befit.backend.service.MotivationalService;
+import discord4j.common.util.Snowflake;
+import discord4j.core.GatewayDiscordClient;
+import discord4j.core.object.entity.channel.MessageChannel;
+import discord4j.core.spec.EmbedCreateSpec;
+import discord4j.rest.util.Color;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+@RequiredArgsConstructor
+@Component
+@Slf4j
+public class ScheduledMotivationalQuote {
+    private final GatewayDiscordClient client;
+    private final MotivationalService motivationalService;
+
+    @Value("${discord.channels.motivational}")
+    private String motivationChannelId;
+
+    @Scheduled(cron = "0 0 8 * * *", zone = "Europe/Amsterdam")
+    public void publishMotivationalQuote() {
+        var message = motivationalService.getRandomQuote();
+        log.info(message.message(), message.author());
+        client
+                .getChannelById(Snowflake.of(motivationChannelId))
+                .ofType(MessageChannel.class)
+                .flatMap(c -> {
+                    var quote = motivationalService.getRandomQuote();
+                    var builder = EmbedCreateSpec.builder()
+                            .title("Quote of the day")
+                            .description(quote.message())
+                            .footer(String.format("- %s", quote.author()), null)
+                            .color(Color.GREEN);
+                    return c.createMessage(builder.build());
+                })
+                .block();
+    }
+}
