4a5a6786198e59ecdfeaa02040829e856fb7d251
jordy <74408184+jordybronowicki37@users.noreply.github.com>
2024-08-18 15:25:42 +0200

    Added measurement categories

---
+++ backend/src/main/java/dev/jb/befit/backend/data/models/MeasurementCategory.java
@@ -0,0 +1,9 @@
+package dev.jb.befit.backend.data.models;
+
+public enum MeasurementCategory {
+    WEIGHT,
+    DISTANCE,
+    SPEED,
+    TIME,
+    AMOUNT
+}
--- backend/src/main/java/dev/jb/befit/backend/data/models/MeasurementTypes.java
+++ backend/src/main/java/dev/jb/befit/backend/data/models/MeasurementTypes.java
@@ -3,21 +3,24 @@ package dev.jb.befit.backend.data.models;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
+import static dev.jb.befit.backend.data.models.MeasurementCategory.*;
+
 @Getter
 @RequiredArgsConstructor
 public enum MeasurementTypes {
-    KG("kg", "kilograms"),
-    G("g", "grams"),
-    KM("km", "kilometers"),
-    M("m", "meters"),
-    CM("cm", "centimeters"),
-    KMH("km/h", "kilometers per hour"),
-    AMOUNT("x", "times"),
-    HOURS("h", "hours"),
-    MINUTES("m", "minutes"),
-    SECONDS("s", "seconds"),
-    BPM("bpm", "beats per minute");
+    KG("kg", "kilograms", WEIGHT),
+    G("g", "grams", WEIGHT),
+    KM("km", "kilometers", DISTANCE),
+    M("m", "meters", DISTANCE),
+    CM("cm", "centimeters", DISTANCE),
+    KMH("km/h", "kilometers per hour", SPEED),
+    AMOUNT("x", "times", MeasurementCategory.AMOUNT),
+    HOURS("h", "hours", TIME),
+    MINUTES("m", "minutes", TIME),
+    SECONDS("s", "seconds", TIME),
+    BPM("bpm", "beats per minute", MeasurementCategory.AMOUNT);
 
     private final String shortName;
     private final String longName;
+    private final MeasurementCategory category;
 }
